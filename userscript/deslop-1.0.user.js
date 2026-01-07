f8528461697a834388a328f1b74900f5f480d89f
NikoboiNFTB <contact@nikoboi.dev>
2026-01-07 12:08:18 +0200

    Automated push

--- userscript/deslop-1.0.user.js
+++ userscript/deslop-1.0.user.js
@@ -13,7 +13,7 @@
     'use strict';
 
     // URL of the blocklist (raw GitHub)
-    const BLOCKLIST_URL = 'https://raw.githubusercontent.com/NikoboiNFTB/DeSlop/refs/heads/main/block/list.txt?token=GHSAT0AAAAAADSVKSWWGC5OUAJC43B3THGW2K6FVEQ';
+    const BLOCKLIST_URL = 'https://raw.githubusercontent.com/NikoboiNFTB/DeSlop/refs/heads/main/block/list.txt';
 
     let blockedChannels = new Set();
 
